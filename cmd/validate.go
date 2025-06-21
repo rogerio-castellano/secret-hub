8600c87fadca2b304d227c7bb835143ca81ad373
Rogerio Castellano <rc_publico-github@yahoo.com.br>
2025-06-21 15:30:41 +0200

    Add a TODO and commented long description of the validate command (missing in the previous commit)

--- cmd/validate.go
+++ cmd/validate.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -14,12 +13,13 @@ import (
 var validateCmd = &cobra.Command{
 	Use:   "validate",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	//TODO: Add a Long description
+	// 	Long: `A longer description that spans multiple lines and likely contains examples
+	// and usage of using your command. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	// Cobra is a CLI library for Go that empowers applications.
+	// This application is a tool to generate the needed files
+	// to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("validate called")
 	},
